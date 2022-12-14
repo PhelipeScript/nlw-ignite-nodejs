745880bfa539a7b1710226ecfe0a71681bc68011
Phelipe Pereira <phelipescript@gmail.com>
2022-12-13 21:39:43 -0300

    settings fixed in VSCode

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from 'node:crypto';
 import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
@@ -23,6 +23,6 @@ export class AppController {
         category,
         recipientId,
       },
-    })
+    });
   }
 }
